2743769639fa532799e6782e310c245c702836f3
HungGarriiix <56022784+HungGarriiix@users.noreply.github.com>
2023-07-17 13:34:42 +0700

    Added already done core files (Sourcing from repo)

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] float rotate = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            rb.AddRelativeForce(Vector3.up * Time.deltaTime * mainThrust);
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            ApplyRotation(rotate);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            ApplyRotation(-rotate);
+    }
+
+    private void ApplyRotation(float rotateThisFrame)
+    {
+        // Rick's solution
+        /*rb.freezeRotation = true;   // freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotateThisFrame);
+        rb.freezeRotation = false;  // unfreezing rotation so the physics system can take over*/
+
+        // Rand's solution
+        rb.angularVelocity += Vector3.forward * Time.deltaTime * rotateThisFrame;
+
+        // Nina's solution (to make the delay longer than Rick's)
+        /*RigidbodyConstraints constraints = rb.constraints;
+        rb.freezeRotation = false;
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotateThisFrame);
+        rb.constraints = constraints;*/
+    }
+}
